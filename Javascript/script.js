21453f217281450fe9f76c882d42f53e8686de15
abhinav731996 <abhinav731996@gmail.com>
2024-11-28 17:55:40 +0530

    practice faukty cal.

--- Javascript/script.js
+++ Javascript/script.js
@@ -4,17 +4,19 @@ console.log("Hello world");
 console.log("code is running...");
 console.log("code is running...");
 
+ //+++++++++ example of var & let/const ++++++++// 
 var a = 2;
 var b = 5;
 var c = "Abhi";
 console.log(a+b+c);
 console.log(typeof a, typeof b, typeof c);
 {
-    let a = 66;                //+++++++++ example of var & let/const ++++++++// 
+    let a = 66;               
     console.log(a);
 }
 console.log(a);
 
+//++++++++ important points to be noted as data types ++++++
 let x = 1;
 let y = "Abhinav";
 let z = 4.4;
@@ -23,7 +25,7 @@ let q = undefined;
 let r = null;
 
 console.log(x,y,z,p,q,r);
-console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);  //++++++++ important points to be noted as data types ++++++
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);  
 
 let o ={
     "name" : "Abhinav",
@@ -31,7 +33,78 @@ let o ={
 }
 console.log(o);
 
-o.salary="100 cr"
+o.salary="100 cr";
 console.log(o);
-o.salary="500cr"
+o.salary="500cr";
 console.log(o);
+
+//+++++++++++++++ If & Else +++++++++++++++++++//
+let age = 45;
+if (age>18) {
+    "you can drive"
+} else {
+    "you cannot drive"
+}
+
+let d = 1;
+console.log(d);
+console.log(d+1);
+console.log(d+2);
+console.log(d+3);
+console.log(d+4);
+
+//++++++++++++++++ Loop +++++++++++++//
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a+i);    
+// }
+
+let obj = {
+    name:"Abhinav",
+    role:"programmer",
+    company:"Devkraft"
+}
+for (const key in obj) {
+    if (Object.hasOwnProperty.call(obj, key)) {
+        const element = obj[key];
+        console.log(key, element);
+    }
+} 
+
+// let i = 0;
+// while (i<6) {
+//         console.log(i);
+//         i++;
+// }
+
+let i = 10;
+do {
+    console.log(i);
+            i++;
+} while (i<6);
+
+function nice(name) {
+    console.log("Hey "+ name +" you are good");;
+    console.log("Hey "+name+" you are nice");;
+    console.log("Hey "+name+" your tshirt is nice");;
+    console.log("Hey "+name+" you are the best");;
+}
+nice("Abhinav")
+nice("Sharma")
+
+function sum(a,b) {
+    return a+b
+}
+result1=sum(3,5);
+result2=sum(4,2);
+result3=sum(7,9);
+console.log("the sum of a + b is ", result1);
+console.log("the sum of a + b is ", result2);
+console.log("the sum of a + b is ", result3);
+
+const funx = (x)=>{
+    console.log("I am a arrow function ", x);
+}
+funx(34);
+funx(25);
+funx(43);
---
+++ practice /Creat a faulty calculator/index.js
@@ -0,0 +1,40 @@
+/* Create a faulty calculator using JavaScript
+
+This faulty calculator does following:
+1. It takes two numbers as input from the user
+2. It perfoms wrong operations as follows:
+
++ ---> -
+* ---> +
+- ---> /
+/ ---> **
+
+
+It performs wrong operation 10% of the times
+
+*/
+
+let random = math.random()
+console.log(random);
+let a = prompt("Enter your first no.")
+let c = prompt("Enter your operation")
+let b = prompt("Enter your secon no.")
+
+let obj = {
+    "+":"-",
+    "*":"/",
+    "/":"**",
+}
+if (random > 0.1) {
+    // Perform correct calculation
+    console.log(`The result is ${a} ${c} ${b}`)
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+    
+}
+
+else {
+    // Perform wrong calculation
+    c = obj[c]
+    alert(`The result is ${eval(`${a} ${c} ${b}`)}`) 
+
+}
