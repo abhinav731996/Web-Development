5d8c7b56a0779742344bf429da7c1ea9efac8299
abhinav731996 <abhinav731996@gmail.com>
2024-11-28 11:32:37 +0530

    javascript

---
+++ Javascript/script.js
@@ -0,0 +1,37 @@
+// alert("hello world");
+
+console.log("Hello world");
+console.log("code is running...");
+console.log("code is running...");
+
+var a = 2;
+var b = 5;
+var c = "Abhi";
+console.log(a+b+c);
+console.log(typeof a, typeof b, typeof c);
+{
+    let a = 66;                //+++++++++ example of var & let/const ++++++++// 
+    console.log(a);
+}
+console.log(a);
+
+let x = 1;
+let y = "Abhinav";
+let z = 4.4;
+let p = true;
+let q = undefined;
+let r = null;
+
+console.log(x,y,z,p,q,r);
+console.log(typeof x,typeof y,typeof z,typeof p,typeof q,typeof r);  //++++++++ important points to be noted as data types ++++++
+
+let o ={
+    "name" : "Abhinav",
+    "Job code" : 56000
+}
+console.log(o);
+
+o.salary="100 cr"
+console.log(o);
+o.salary="500cr"
+console.log(o);
