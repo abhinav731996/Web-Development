16f62fdcda19e0b42a16716a37e760ba90a94931
abhinav731996 <abhinav731996@gmail.com>
2024-12-04 11:09:12 +0530

    Dom

---
+++ Javascript/dom/script.js
@@ -0,0 +1,9 @@
+document.title = "Abhinav's web site"
+document.body.style.backgroundColor = "green"
+console.log("Hello world");
+
+let abhibox = document.getElementsByClassName("boxes")
+console.log(abhibox);
+abhibox[2].style.backgroundColor = "red";
+
+document.getElementById("blue").style.backgroundColor="blue";
