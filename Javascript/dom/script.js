a07bfc308a67d0bb198d13701b035e58dec4f730
abhinav731996 <abhinav731996@gmail.com>
2024-12-04 15:31:07 +0530

    dom insert

--- Javascript/dom/script.js
+++ Javascript/dom/script.js
@@ -7,3 +7,11 @@ console.log(abhibox);
 abhibox[2].style.backgroundColor = "red";
 
 document.getElementById("blue").style.backgroundColor="blue";
+
+let div = document.createElement("div");
+div.innerHTML = "Created by <b>Abhi </b>" 
+div.setAttribute("class", "created");
+document.querySelector(".bjboxes").append(div);
+
+let cont = document.querySelector(".bjboxes");
+cont.insertAdjacentHTML("afterbegin", "This is inserted ")
